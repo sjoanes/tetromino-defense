58fa34328e6b8b83952d3e0c26df52d0d1d137b3
Savio Joanes <savio.joanes@gmail.com>
2017-05-02 18:15:04 -0400

    Initial commit

---
+++ Assets/Siege.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class Siege : MonoBehaviour {
+
+	public Transform goal;
+
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position; 
+	}
+}
